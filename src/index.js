d482ef97d1e1b69da7ff11f5253d53a2d17da48d
unknown <iaguiarg@hotmail.com>
2019-07-29 15:20:16 -0500

    Modifique css

--- src/index.js
+++ src/index.js
@@ -232,3 +232,17 @@ function addLikes(id, likes) {
     });
 }
 console.log(addLikes);
+
+
+// $('.modal-content').resizable({
+//   //alsoResize: ".modal-dialog",
+//   minHeight: 300,
+//   minWidth: 300
+// });
+// $('.modal-dialog').draggable();
+
+// $('#myModal').on('show.bs.modal', function() {
+//   $(this).find('.modal-body').css({
+//     'max-height': '100%'
+//   });
+// });
